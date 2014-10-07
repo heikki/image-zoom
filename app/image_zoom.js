dfd510d07867cdbee250f6480840de3b750ab201
Heikki Ylönen <heikki74@gmail.com>
2014-10-07 20:12:57 +0300

    Rename directive. Add image toggle.

--- app/image_zoom.js
+++ app/image_zoom.js
@@ -1,24 +1,27 @@
-App.directive('zoomBox', function($document, $window) {
+App.directive('imageZoom', function($document, $window) {
 	return {
 		restrict: 'E',
 		replace: true,
-		transclude: true,
-		template:
-			'<div class="zoom-box">' +
-			'    <div class="zoom-box-content" ng-transclude></div>' +
-			'</div>',
+		template: '<div class="image-zoom"><img src="{{ image }}"></div>',
+		scope: {
+			image: '@'
+		},
 		link: function(scope, element) {
-
-			var content = element.children('div');
 
 			var pan = Victor(0, 0);
 
 			var zoom = 0;
 
+			var content = element.children('img').css('width', '100%').on('load', function() {
+				content.css('width', 'auto');
+				zoom = 0;
+				update();
+			});
+
 			function update() {
 
 				// Set container height to match content aspect ratio
-				element.css({ height: ((content[0].offsetHeight / content[0].offsetWidth) * element[0].offsetWidth) + 'px' });
+				element.css('height', ((content[0].offsetHeight / content[0].offsetWidth) * element[0].offsetWidth) + 'px');
 
 				var elementSize = Victor(element[0].offsetWidth, element[0].offsetHeight);
 				var contentSize = Victor(content[0].offsetWidth, content[0].offsetHeight);
@@ -48,10 +51,7 @@ App.directive('zoomBox', function($document, $window) {
 				});
 			}
 
-			$window.requestAnimationFrame(update);
-
 			element.on('click', function(event) {
-				zoom = 0;
 				if (document.webkitFullscreenElement === null) {
 					event.currentTarget.webkitRequestFullscreen();
 				} else if (document.webkitFullscreenElement === event.currentTarget) {
@@ -72,13 +72,17 @@ App.directive('zoomBox', function($document, $window) {
 				update();
 			});
 
-			$document.on('webkitfullscreenchange', update);
+			$document.on('webkitfullscreenchange', function() {
+				zoom = 0;
+				update();
+			});
 
 			angular.element($window).on('resize', update);
 
 			element.on('$destroy', function() {
 				element.off();
-				$document.off('webkitfullscreenchange', update);
+				content.off();
+				$document.off('webkitfullscreenchange');
 				angular.element($window).off('resize', update);
 			});
 
--- app/index.js
+++ app/index.js
@@ -2,6 +2,18 @@ window.App = angular.module('image-zoom', []);
 
 App.controller('MainCtrl', function() {
 
-	// var vm = this;
+	var images = [
+		'test.jpg',
+		'test-left.jpg',
+		'test-right.jpg'
+	];
+
+	this.image = images.shift();
+
+	this.changeImage = function() {
+		var next = images.shift();
+		images.push(this.image);
+		this.image = next;
+	};
 
 });
